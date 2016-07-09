d6004f2e1b6c796f507983df5926559eedf46be5
Sean Chow <scho@tugo.com>
2016-07-08 17:20:15 -0700

    Initial commit of project

---
+++ collections/Recipes.js
@@ -0,0 +1,62 @@
+Recipes = new Mongo.Collection('recipes');
+
+//Rules for who is allowed to do certain actions
+Recipes.allow({
+  insert: function(userId, doc) {
+    return !!userId; // returns true if the userId exists
+  }
+});
+
+Ingredient = new SimpleSchema({
+  name: {
+    type: String
+  },
+  amount: {
+    type: String
+  }
+}); 
+
+RecipeSchema = new SimpleSchema({
+  // All fields are required by default
+  name: {
+    type: String,
+    label: "Name"
+  },
+  description: {
+    type: String,
+    label: "Description"
+  },
+  ingredients: {
+    type: [Ingredient]
+  },
+  inMenu: {
+    type: Boolean,
+    defaultValue: false,
+    optional: true,
+    autoform: {
+      type: "hidden"
+    }
+  },
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function() {
+      return this.userId;
+    },
+    autoform: {
+      type: "hidden"
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: "Created At",
+    autoValue: function() {
+      return new Date();
+    },
+    autoform: {
+      type: "hidden"
+    }
+  }
+});
+
+Recipes.attachSchema(RecipeSchema);
